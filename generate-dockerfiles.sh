e4b1d9c7d6202722564bdb50c1d90a1dfa845d8d
bfren <ben@bcgdesign.com>
2022-02-10 17:49:12 +0000

    v4.0.0 (#59)
    Major changes
    * **Breaking changes** major rewrite of S6 Overlay for v3.0.0.0 - see base image and documentation for more information
    
    Documentation changes
    * Updating copyright year to 2022

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="3.2.3"
+BASE_REVISION="4.0.0"
 echo "Base: ${BASE_REVISION}"
 
 ASPNET_VERSIONS="3.1 5.0 6.0"
@@ -15,6 +15,7 @@ for V in ${ASPNET_VERSIONS} ; do
 
     DOCKERFILE=$(docker run \
         -v ${PWD}:/ws \
+        -e BF_DEBUG=0 \
         bfren/alpine esh \
         "/ws/Dockerfile.esh" \
         BASE_REVISION=${BASE_REVISION} \
