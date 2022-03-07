7fbe56ef02eade83a4eaa516d30f0d748b7476dc
bfren <ben@bcgdesign.com>
2022-03-07 09:18:47 +0000

    v4.1.6 (#74)
    Minor updates
    * Using latest base images
    
    Development updates
    * Adding pull.sh and run.sh
    
    Build updates
    * Publish GitHub packages

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+IMAGE=`cat VERSION`
+ASPNET=${1:-6.0}
+
+docker buildx build \
+    --build-arg BF_IMAGE=aspnet \
+    --build-arg BF_VERSION=${IMAGE} \
+    -f ${ASPNET}/Dockerfile \
+    -t aspnet${ASPNET}-dev \
+    . \
+    && \
+    docker run -it aspnet${ASPNET}-dev sh
