6fe59674192be17e36319cfa98ed0bba438b4c6b
bfren <bf@bfren.dev>
2022-12-11 21:23:02 +0000

    Using laetst base images

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="4.4.1"
+BASE_REVISION="4.4.2"
 echo "Base: ${BASE_REVISION}"
 
-ASPNET_VERSIONS="3.1 6.0 7.0"
+ASPNET_VERSIONS="3.1 6 7"
 for V in ${ASPNET_VERSIONS} ; do
 
     echo "ASP.NET ${V}"
@@ -20,7 +20,7 @@ for V in ${ASPNET_VERSIONS} ; do
         "/ws/Dockerfile.esh" \
         BASE_REVISION=${BASE_REVISION} \
         ALPINE_MINOR=${ALPINE_MINOR} \
-        ASPNET_MINOR=${V}
+        ASPNET_VERSION=${V}
     )
 
     echo "${DOCKERFILE}" > ./${V}/Dockerfile
