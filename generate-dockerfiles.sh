3f05c430d6abaa997cf720329087c83fbc74ab0a
bfren <bf@bfren.dev>
2023-09-13 09:44:37 +0000

    v4.5.29 (#150)
    Minor updates
    * Using latest base images
    
    Development updates
    * Adding --load to run.sh

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_REVISION="4.5.6"
+BASE_REVISION="4.5.7"
 echo "Base: ${BASE_REVISION}"
 
 ASPNET_VERSIONS="6 7 8"
--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ IMAGE=`cat VERSION`
 ASPNET=${1:-6.0}
 
 docker buildx build \
+    --load \
     --build-arg BF_IMAGE=aspnet \
     --build-arg BF_VERSION=${IMAGE} \
     -f ${ASPNET}/Dockerfile \
