ae5fd341db05f98e1e8b79a350fdac94a97c939f
bfren <bf@bfren.dev>
2023-10-20 14:54:27 +0100

    Renaming to BF_ASPNET_ASSEMBLY

--- run.sh
+++ run.sh
@@ -13,4 +13,4 @@ docker buildx build \
     -t aspnet${ASPNET}-dev \
     . \
     && \
-    docker run -it -e ASPNET_ASSEMBLY=/tmp/null aspnet${ASPNET}-dev sh
+    docker run -it -e BF_ASPNET_ASSEMBLY=/tmp/null aspnet${ASPNET}-dev sh
