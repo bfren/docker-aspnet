60ff7fad653a373885362195e5e255624323e064
bfren <bf@bfren.dev>
2023-10-21 15:50:45 +0100

    Set run.sh path temporarily

--- run.sh
+++ run.sh
@@ -5,12 +5,10 @@ ASPNET=${1:-7}
 
 docker buildx build \
     --load \
-    --no-cache \
-    --progress plain \
     --build-arg BF_IMAGE=aspnet \
     --build-arg BF_VERSION=${IMAGE} \
     -f ${ASPNET}/Dockerfile \
     -t aspnet${ASPNET}-dev \
     . \
     && \
-    docker run -it -e BF_DEBUG=1 -e BF_ASPNET_ASSEMBLY=bf.bfren_dev.dll -p "5000:5000" -v /mnt/d/Temp/Publish/bfren:/app/live aspnet${ASPNET}-dev sh
+    docker run -it -e BF_DEBUG=1 -e BF_ASPNET_ASSEMBLY=bf.bfren_dev.dll -p "5000:5000" -v /home/bcg/docker/aspnet/v/live:/app/live aspnet${ASPNET}-dev sh
