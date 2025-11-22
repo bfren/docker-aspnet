fcaedb5235eacc8b35a2f7ca9851555b0ecde0e1
bfren <bf@bfren.dev>
2025-11-22 18:04:32 +0000

    Removing support for .NET 6 and 7 (EOL)

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -7,7 +7,7 @@ docker pull bfren/alpine
 BASE_VERSION="6.0.0"
 echo "Base: ${BASE_VERSION}"
 
-ASPNET_VERSIONS="6 7 8 9 10"
+ASPNET_VERSIONS="8 9 10"
 for V in ${ASPNET_VERSIONS} ; do
 
     echo "ASP.NET ${V}"
